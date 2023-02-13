0fd7522d41c5eb0a31bbfbe1a9c66cdbdf833f08
ivan-verbnyak <kadet717@ya.ru>
2023-02-13 22:10:21 +0500

    metodArray

---
+++ Examples/example010_metodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 3, 22, 5, 6, 798, 74, 2, 22 };
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+index++;
+
+}
